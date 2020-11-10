ca15f5b9d73647a1c4fb092cb96cf696468484ca
Gal Havivi <gal.havivi@oath.com>
2020-11-10 09:08:13 +0200

    first-commit

---
+++ react-form/precache-manifest.4e153767f3e88c644b8752eb2beea056.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20144d2492ed71f0443a3353d65b47ee",
+    "url": "https://galhavivi.github.io/cofi/react-form/index.html"
+  },
+  {
+    "revision": "eca2da04923497b5491f",
+    "url": "https://galhavivi.github.io/cofi/react-form/static/css/main.99903ae4.css"
+  },
+  {
+    "revision": "eca2da04923497b5491f",
+    "url": "https://galhavivi.github.io/cofi/react-form/static/js/main.75ace816.js"
+  },
+  {
+    "revision": "07ab01bb7bfde7c15bdfda6cac90fd10",
+    "url": "https://galhavivi.github.io/cofi/react-form/static/js/main.75ace816.js.LICENSE.txt"
+  },
+  {
+    "revision": "8387712a669dfe22884f656d81857331",
+    "url": "https://galhavivi.github.io/cofi/react-form/static/media/logo.8387712a.svg"
+  }
+]);
---
+++ react-layout/precache-manifest.6bde9c6d6d7364e53df7293009cf146c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29ff1273d612abeddbeeeceb163d8782",
+    "url": "https://galhavivi.github.io/cofi/react-layout/index.html"
+  },
+  {
+    "revision": "d3695fff4d4cfe468da3",
+    "url": "https://galhavivi.github.io/cofi/react-layout/static/css/main.99903ae4.css"
+  },
+  {
+    "revision": "d3695fff4d4cfe468da3",
+    "url": "https://galhavivi.github.io/cofi/react-layout/static/js/main.cd9dfa15.js"
+  },
+  {
+    "revision": "07ab01bb7bfde7c15bdfda6cac90fd10",
+    "url": "https://galhavivi.github.io/cofi/react-layout/static/js/main.cd9dfa15.js.LICENSE.txt"
+  },
+  {
+    "revision": "8387712a669dfe22884f656d81857331",
+    "url": "https://galhavivi.github.io/cofi/react-layout/static/media/logo.8387712a.svg"
+  }
+]);
